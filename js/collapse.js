a9a03642254e4e73e36fdd73dbe510835b4f4fc9
nyawonkulu <mancue.makhaye@gmail.com>
2018-08-16 09:20:04 +0200

    addition of collapse, index, agency

---
+++ js/collapse.js
@@ -0,0 +1,25 @@
+let myLabels = document.querySelectorAll('.lbl-toggle');
+
+Array.from(myLabels).forEach(label => {
+  label.addEventListener('keydown', e => {
+    // 32 === spacebar
+    // 13 === enter
+    if (e.which === 32 || e.which === 13) {
+      e.preventDefault();
+      label.click();
+    };
+  });
+});
+
+let myLabels = document.querySelectorAll('.lbl-toggle-2');
+
+Array.from(myLabels).forEach(label => {
+  label.addEventListener('keydown', e => {
+    // 32 === spacebar
+    // 13 === enter
+    if (e.which === 32 || e.which === 13) {
+      e.preventDefault();
+      label.click();
+    };
+  });
+});
